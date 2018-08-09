82393dd12923d4580ead4333520ae63d637cdc5e
sebastian <sebashaedo@hotmail.com>
2018-08-09 14:04:23 -0300

    Initial commit

---
+++ ing/contacto.php
@@ -0,0 +1,52 @@
+﻿<?php
+/**
+ * @version 1.0
+ */
+
+require("class.phpmailer.php");
+require("class.smtp.php");
+
+// Valores enviados desde el formulario
+if ( !isset($_POST["nombre"]) || !isset($_POST["email"]) || !isset($_POST["mensaje"]) ) {
+    die ("Es necesario completar todos los datos del formulario");
+}
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "c0370404.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "ventas@flymartinair.com.ar";  // Mi cuenta de correo
+$smtpClave = "Fly2017";  // Mi contraseña
+
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "ventas@flymartinair.com.ar";
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 587; 
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+// VALORES A MODIFICAR //
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $nombre;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+
+$mail->Subject = "Consultas en linea - Martinair Aviation"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "{$mensajeHtml} <br /><br />Consultas en linea<br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n Consultas en linea"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$estadoEnvio = $mail->Send(); 
+if($estadoEnvio){
+    echo "El correo fue enviado correctamente.";
+} else {
+    echo "Ocurrió un error inesperado.";
+}
